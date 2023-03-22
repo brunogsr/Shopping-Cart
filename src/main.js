d7a945ff543e67c847a97081154414d9e29754d2
brunogsr <brunogsr https://github.com/brunogsr>
2023-03-22 19:17:48 -0300

    Testando se req 3 passou

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
-fetchProductsList('computador');
+const Poggers = async () => {
+  const listResults = await fetchProductsList('computador');
+  const sectionHtml = document.getElementsByClassName('products');
+  // console.log(listResults);
+  listResults.forEach((produto) => {
+    sectionHtml[0].appendChild(createProductElement(produto));
+  });
+};
+
+Poggers();
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
