6a9d35029fb25663a990b58e6ae8f2e3bd442653
brunogsr <brunogsr https://github.com/brunogsr>
2023-03-23 23:02:35 -0300

    Finalizando requisito 8

--- src/main.js
+++ src/main.js
@@ -1,15 +1,31 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+import { fetchProductsList, fetchProduct } from './helpers/fetchFunctions';
+import { createProductElement, createCartProductElement } from './helpers/shopFunctions';
+import { saveCartID } from './helpers/cartFunctions';
 
-const Poggers = async () => {
+let buttonAddCart;
+
+const poggers = async (event) => {
+  const productID = event.target.parentNode.firstChild.innerText;
+  const carrinhoList = document.querySelector('.cart__products');
+  // console.log(carrinhoList);
+  // console.log(eventHtml);
+  const product = await fetchProduct(productID);
+  const cartProduct = createCartProductElement(product);
+  carrinhoList.appendChild(cartProduct);
+  saveCartID(productID);
+};
+
+const productScreen = async () => {
   try {
     const listResults = await fetchProductsList('computador');
     const sectionHtml = document.getElementsByClassName('products');
     listResults.forEach((produto) => {
       sectionHtml[0].appendChild(createProductElement(produto));
     });
+    buttonAddCart = document.querySelectorAll('.product__add');
+    buttonAddCart.forEach((element) => element.addEventListener('click', poggers));
     const loadingDOM = document.getElementsByClassName('loading')[0];
     loadingDOM.parentElement.removeChild(loadingDOM);
   } catch (error) {
@@ -23,6 +39,10 @@ const Poggers = async () => {
   }
 };
 
-Poggers();
-
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+// console.log(getSavedCartIDs());
+
+window.onload = () => {
+  productScreen();
+};
